cb8d3dd956b59471c549d4e5f2aecda4862f9107
CoopNich <CoopNich92@gmail.com>
2020-04-28 14:16:12 -0500

    part 1 in progress

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from . import Cohort, Student, Exercise, Instructor
+from NSS import Cohort, Student, Exercise, Instructor
+
+
+easy_exercise = Exercise("Celebrity Tribute", "HTML")
+medium_exercise = Exercise("WTN", "Javascript")
+hard_exercise = Exercise("Nutshell", "Javascript")
+mega_hard_exercise = Exercise("Capstone", "Javascript/React")
+
+cohort_36 = Cohort(36)
+cohort_37 = Cohort(37)
+cohort_38 = Cohort(38)
+
+cooper = Student("Cooper", "Nichols", "Coop", cohort_38)
+
+andy = Instructor("Andy", "Collins", "TGWG", cohort_38)
+
+
+
+
 
 
