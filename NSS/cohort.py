c5ff01e8f404b0d2d66a1b1f6389fca20842ae6c
CoopNich <CoopNich92@gmail.com>
2020-05-02 15:44:39 -0500

    refactored for inheritance, and completed creation of people

--- NSS/cohort.py
+++ NSS/cohort.py
@@ -2,5 +2,11 @@ class Cohort:
 
     def __init__(self, name):
         self.name = name
-        self.students = set()
-        self.instructors = set()
+        self.students = list()
+        self.instructors = list()
+
+    def add_student(self, *student):
+        self.students.append(student)
+
+    def add_instructor(self, *instructor):
+        self.instructors.append(instructor)
--- NSS/instructor.py
+++ NSS/instructor.py
@@ -1,13 +1,11 @@
 from .student import Student
+from .nss_person import NSSPerson
 
-class Instructor:
+class Instructor(NSSPerson):
     def __init__(self, first_name, last_name, slack_handle, cohort):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack_handle = slack_handle
-        self.cohort = cohort
+        super().__init__(first_name, last_name, slack_handle, cohort)
         self.speciality = ""
     
     def add_exercise(self, student, exercise):
-        student.current_exercises.update(exercise)
+        student.current_exercises.append(exercise)
         
